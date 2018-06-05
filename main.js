b2ac760c24f11bee7cff4a6066fd582f590b40e2
Maria <mari4w04@stud.kea.dk>
2018-06-05 10:54:16 +0200

    delete btn working

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-
 const database = firebase.database();
 const headerEl = document.querySelector("#form_header");
 const descriptionEl = document.querySelector("#form_description");
-
 const form = document.querySelector("form");
 const template = document.querySelector("#noteTemplate").content;
 const app = document.querySelector("#app");
@@ -21,15 +19,28 @@ form.addEventListener("submit", function(e){
 });
 
 //listen for new data
-
 database.ref("notes/").on("child_added", (snapshot)=>{
-    console.log(snapshot);
+    //console.log(snapshot);
     const key = snapshot.key;
     const data = snapshot.val();
     // console.log(key);
     // console.log(data);
     const clone = template.cloneNode(true);
+    clone.querySelector("article").dataset.key=key;
     clone.querySelector("h1").textContent = data.header;
     clone.querySelector("div.description").textContent = data.description;
+    clone.querySelector("#edit-btn").addEventListener("click", e=>{
+
+    });
+    clone.querySelector("#delete-btn").addEventListener("click", e=>{
+        database.ref("notes/"+key).remove();
+    });
     app.appendChild(clone);
+});
+
+//listen for removal of data child_removed
+database.ref("notes/").on("child_removed", (snapshot)=>{
+    const key = snapshot.key;
+    let el = document.querySelector("article[data-key="+key+"]");
+    el.remove();
 });
